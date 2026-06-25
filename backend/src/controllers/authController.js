ae0655fc0a1f927b959cf4b31bb732c64f1fa766
Ipsita Maharana <ipsitamaharana70@gmail.com>
2026-06-25 17:10:46 +0530

    Day 3 - user registration with bcrypt and Supabase

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,36 @@
-const registerUser = (req, res) => {
+const bcrypt = require("bcryptjs");
+const supabase = require("../config/supabase");
+
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({
+      message: "All fields are required",
+    });
+  }
+  
+
+const hashedPassword = await bcrypt.hash(password, 10);
+
+const { data, error } = await supabase
+  .from("users")
+  .insert([
+    {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  ]);
+
+if (error) {
+  console.log(error);
+
+  return res.status(500).json({
+    message: error.message,
+  });
+}
+
   res.json({
     message: "Register Controller Working",
   });
